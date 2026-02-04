4f8a6e6db9b31b7834082655ced5dfcf90ece2d8
Sasha Chambers <sashachambers10.12@gmail.com>
2026-02-04 22:19:22 +1300

    Used isLoading and isError to check if data is available and able to use.

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,5 +1,36 @@
 import { Link } from 'react-router-dom'
+import { useManifestData } from '../hooks/useAPIs'
+
+// To Do:
+// - Fetch API data on startup
+// - If fetch fails, show error message but allow user to continue to server selector, otherwise continue normally.
+// - The fetch limit is 600 requests per 10 minutes, so cache the result in local storage with a timestamp.
+
+// - Style the page nicely.
+
 export default function Home() {
+  const { isLoading, isError } = useManifestData()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error fetching Minecraft versions.</h1>
+        <p>
+          There was an issue fetching the Minecraft version data. You can
+          continue to the server selector, but some features may not work as
+          expected.
+        </p>
+        <Link to="select-server">
+          <button>Go to Server Selector</button>
+        </Link>
+      </div>
+    )
+  }
+
   return (
     <>
       <header>
@@ -8,10 +39,10 @@ export default function Home() {
       <main>
         <h2>Choose:</h2>
         <Link to="select-server">
-          <button>option 1</button>
+          <button>Select Server</button>
         </Link>
         <Link to="create-server">
-          <button>option 2</button>
+          <button>Create Server</button>
         </Link>
       </main>
       <footer>
