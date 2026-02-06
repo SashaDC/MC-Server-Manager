ca5feef9e31b54ef5c3736da1741aeeee36ad8e7
Sasha Chambers <sashachambers10.12@gmail.com>
2026-02-06 14:36:54 +1300

    Upgraded the filter so its more reactive and uses actual data from the API, Also prepped for next stage: Server Creation Logic.

--- client/pages/CreateServer.tsx
+++ client/pages/CreateServer.tsx
@@ -1,35 +1,59 @@
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import ServerCreationLogic from '../components/CreateServerLogic'
+
+interface Props {
+  id: string
+  name: string
+  type: string
+}
 
 export default function CreateServer() {
-  // Test data, replace with actual API data later
+  // This gets the data passed from the Home component via the Link's state prop.
+  const location = useLocation()
+  const data = location.state
+
+  // Filter state for different version types (release, snapshot, other)
   const [filter, setFilter] = useState('release')
 
   const handleFilterChange = (newFilter: string) => {
     setFilter(newFilter)
   }
-  const data = [
-    { id: '1.19.4', name: '1.19.4', type: 'release' },
-    { id: '1.20', name: '1.20', type: 'snapshot' },
-    { id: '1.20.1', name: '1.20.1', type: 'other' },
-    { id: '1.20.2', name: '1.20.2', type: 'release' },
-  ]
+
+  const filteredData = data.versions.filter((version: { type: string }) => {
+    if (filter === 'release') {
+      return version.type === 'release'
+    }
+    if (filter === 'snapshot') {
+      return version.type === 'snapshot'
+    }
+    if (filter === 'other') {
+      return (
+        version.type === 'old_alpha' ||
+        version.type === 'old_beta' ||
+        version.type === 'pre_release'
+      )
+    }
+  })
+
+  const handleSubmit = () => {
+    // Check to see if everything is valid, then create the server with the selected options.
+    // Details = { serverName, Object where id === selected version and then get the url from that object }
+    ServerCreationLogic(Details)
+  }
 
   return (
     <>
       <p>Make a new server</p>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="serverName">Server Name:</label>
         <input type="text" id="serverName" name="serverName" />
         <br />
         <label htmlFor="serverVersion">Server Version:</label>
         <select id="serverVersion" name="serverVersion">
-          {data.map((version) => (
-            <option
-              key={version.id}
-              value={version.id}
-              style={{ display: version.type === filter ? 'block' : 'none' }}
-            >
-              {version.name}
+          {filteredData.map((version: Props) => (
+            <option key={version.id} value={version.id}>
+              {version.id}
             </option>
           ))}
         </select>
