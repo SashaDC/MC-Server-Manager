b6ae02ace2bb699235568e5ec89e9b1bd5223655
Sasha Chambers <sashachambers10.12@gmail.com>
2026-02-06 13:24:56 +1300

    Made basic form with test data to map through as options for the select input.

--- client/pages/CreateServer.tsx
+++ client/pages/CreateServer.tsx
@@ -1,7 +1,44 @@
 export default function CreateServer() {
+  // Test data, replace with actual API data later
+  const data = [
+    { id: '1.19.4', name: '1.19.4', type: 'release' },
+    { id: '1.20', name: '1.20', type: 'snapshot' },
+    { id: '1.20.1', name: '1.20.1', type: 'other' },
+    { id: '1.20.2', name: '1.20.2', type: 'release' },
+  ]
+
   return (
     <>
       <p>Make a new server</p>
+      <form action="">
+        <label htmlFor="serverName">Server Name:</label>
+        <input type="text" id="serverName" name="serverName" />
+        <br />
+        <label htmlFor="serverVersion">Server Version:</label>
+        <select id="serverVersion" name="serverVersion">
+          {data.map((version) => (
+            <option key={version.id} value={version.id}>
+              {version.name}
+            </option>
+          ))}
+        </select>
+        <input
+          type="radio"
+          id="release"
+          name="option"
+          value="release"
+          defaultChecked
+        />
+        <label htmlFor="release">Release</label>
+        <input type="radio" id="snapshot" name="option" value="snapshot" />
+        <label htmlFor="snapshot">Snapshot</label>
+        <input type="radio" id="other" name="option" value="other" />
+        <label htmlFor="other">Other</label>
+
+        <br />
+
+        <button type="submit">Create Server</button>
+      </form>
     </>
   )
 }
